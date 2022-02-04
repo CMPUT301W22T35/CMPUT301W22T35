c222f5c437a099b1694984803311a34ecde420a0
Yuchen Qian <yq4@ualberta.ca>
2022-02-03 20:34:52 -0700

    second commit

--- app/src/main/java/com/example/cmput301w22t35/Shape.java
+++ app/src/main/java/com/example/cmput301w22t35/Shape.java
@@ -4,7 +4,6 @@ public abstract class Shape {
     protected Integer x;
     protected Integer y;
 
-    String color = "blue";
     Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
