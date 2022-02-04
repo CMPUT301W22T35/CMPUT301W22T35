8c90b90069be17bb7478c990a6ff7aa6ef88ecca
KarlHanEdn <tianming@ualberta.ca>
2022-02-03 21:10:24 -0700

    color deep blue

--- app/src/main/java/com/example/cmput301w22t35/Shape.java
+++ app/src/main/java/com/example/cmput301w22t35/Shape.java
@@ -3,6 +3,7 @@ package com.example.cmput301w22t35;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
+    String color = "deep blue";
 
     Shape(Integer x, Integer y) {
         this.x = x;
