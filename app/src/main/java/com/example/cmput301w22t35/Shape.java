20b0b395006b7676ca5ea69c9bfb9c70576e7aef
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-02-03 20:12:08 -0700

    Added Color field

--- app/src/main/java/com/example/cmput301w22t35/Shape.java
+++ app/src/main/java/com/example/cmput301w22t35/Shape.java
@@ -3,7 +3,8 @@ package com.example.cmput301w22t35;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-
+    protected String color = "blue";
+    
     Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
