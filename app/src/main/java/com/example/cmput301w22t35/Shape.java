e8e516bab0bd94f4fc166f9e2b00e564033d7b35
Evan <mrawsomazing@gmail.com>
2022-02-04 03:24:21 -0700

    color

--- app/src/main/java/com/example/cmput301w22t35/Shape.java
+++ app/src/main/java/com/example/cmput301w22t35/Shape.java
@@ -3,9 +3,12 @@ package com.example.cmput301w22t35;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
+    String color = "blue";
 
     Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
     }
+
+
 }
