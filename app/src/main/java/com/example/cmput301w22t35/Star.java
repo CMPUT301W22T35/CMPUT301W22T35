aea3c4812dfa6e8c9413a9042bbde44d493800db
Evan <mrawsomazing@gmail.com>
2022-02-04 03:21:19 -0700

    color

--- app/src/main/java/com/example/cmput301w22t35/Star.java
+++ app/src/main/java/com/example/cmput301w22t35/Star.java
@@ -1,2 +1,8 @@
-package com.example.cmput301w22t35;public class Star {
+package com.example.cmput301w22t35;
+
+public class Star extends Shape{
+    Star(Integer x, Integer y) {
+        super(x, y);
+    }
+
 }
