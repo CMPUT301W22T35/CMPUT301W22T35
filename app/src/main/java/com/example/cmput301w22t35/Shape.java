16f3c680b43fd2ee805d5851bf3b29bc7bf3a818
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-02-04 00:44:40 -0700

    Update Shape.java
    Edited to create a conflict for jaime

--- app/src/main/java/com/example/cmput301w22t35/Shape.java
+++ app/src/main/java/com/example/cmput301w22t35/Shape.java
@@ -3,7 +3,7 @@ package com.example.cmput301w22t35;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR_";
     
     Shape(Integer x, Integer y) {
         this.x = x;
