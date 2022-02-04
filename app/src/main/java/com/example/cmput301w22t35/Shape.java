403451533c865e3bdadc9c4953bb4fa3719059ad
Althea Geollegue <ageolleg@ualberta.ca>
2022-02-03 20:29:43 -0700

    add color

--- app/src/main/java/com/example/cmput301w22t35/Shape.java
+++ app/src/main/java/com/example/cmput301w22t35/Shape.java
@@ -3,6 +3,7 @@ package com.example.cmput301w22t35;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
+    String color = "red";
 
     Shape(Integer x, Integer y) {
         this.x = x;
