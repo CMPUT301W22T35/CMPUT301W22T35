6e35a6b373fa6b34f09615c000e76edfb1cb3b71
Jayden Dumouchel <jdumouch@ualberta.ca>
2022-02-03 19:26:30 -0700

    Added Shape.java

---
+++ app/src/main/java/com/example/cmput301w22t35/Shape.java
@@ -0,0 +1,11 @@
+package com.example.cmput301w22t35;
+
+public abstract class Shape {
+    protected Integer x;
+    protected Integer y;
+
+    Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
